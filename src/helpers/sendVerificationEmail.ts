5c6c82b16719fbb16c62a1cb21c60bb9fd532742
Saadat-B <saadatbadgujar@gmail.com>
2024-07-29 11:29:21 +0530

    add resend and routes with db connection

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,31 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+import { url } from "inspector";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "TrueFeedback | Verification code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+
+    return {
+      success: true,
+      message: "Verification email send successfully",
+    };
+  } catch (emailError) {
+    console.log("Error sending verification email", emailError);
+    return {
+      success: false,
+      message: "Failed to send verification email",
+    };
+  }
+}
